f176afb69e5973b0fa32a7cb8ecd226e3e718629
Vitaly <vet.it08@gmail.com>
2016-10-20 12:16:26 +0300

    fix test db migration

--- test/mysql.sql
+++ test/mysql.sql
@@ -1,13 +1,12 @@
-CREATE DATABASE test
+CREATE DATABASE IF NOT EXISTS `test`;
 
-CREATE TABLE test
+CREATE TABLE `test`.`test`
 (
 id int NOT NULL AUTO_INCREMENT,
 LastName varchar(255),
 FirstName varchar(255),
 gender ENUM('Man', 'Woman'),
 PRIMARY KEY (id)
-)
+);
 
-INSERT INTO `test` (`LastName`, `FirstName`, `gender`) VALUES ('Ivan', 'Ivanov', 'Man');
-
+INSERT INTO `test`.`test` (`LastName`, `FirstName`, `gender`) VALUES ('Ivan', 'Ivanov', 'Man');
